b4376c394a0c4f775c5c22f3080e3f6edc11e151
Inna <nnkrmlv@gmail.com>
2021-02-04 03:23:44 +0300

    Поправлен .gitignore, добавлено пространство имен для mainapp.urls

--- mainapp/views.py
+++ mainapp/views.py
@@ -17,7 +17,7 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
+def products(request, id=None):
 
     context = {
         'products': Product.objects.all(),
