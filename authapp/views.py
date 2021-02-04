9e5e6527e79110f765e7ea2ee91154114f8338ae
Inna <nnkrmlv@gmail.com>
2021-02-04 12:59:05 +0300

    Добавлено приложение authapp

---
+++ authapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, 'authapp/login.html')
+
+
+def register(request):
+    return render(request, 'authapp/register.html')
