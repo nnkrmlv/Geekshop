7b90a0d273b086d312c918d5f25d3b7e1e1922a4
Inna <nnkrmlv@gmail.com>
2021-02-07 23:47:26 +0300

    Added forms for profile

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from authapp.forms import UserLoginForm, UserRegisterForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from authapp.models import User
 from django.contrib import auth
 from django.urls import reverse
@@ -39,4 +39,12 @@ def logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 def profile(request):
-     return render(request, 'authapp/profile.html')
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'authapp/profile.html', context)
