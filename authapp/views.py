0008319fe0745c785f1fc3202a074677a1c9b88c
Inna <nnkrmlv@gmail.com>
2021-02-07 10:49:46 +0300

    Добавлен profile

--- authapp/views.py
+++ authapp/views.py
@@ -37,3 +37,6 @@ def register(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def profile(request):
+     return render(request, 'authapp/profile.html')
