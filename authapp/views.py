4e96252be9aa8e96157c35f0e16e1915a753f72b
Inna <nnkrmlv@gmail.com>
2021-02-08 04:13:48 +0300

    Added basket

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
-from authapp.models import User
+from basket.models import Basket
 from django.contrib import auth
 from django.urls import reverse
 
@@ -46,5 +46,9 @@ def profile(request):
             return HttpResponseRedirect(reverse('auth:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form}
+    context = {
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+     }
+
     return render(request, 'authapp/profile.html', context)
